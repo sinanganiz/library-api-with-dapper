985d6209669ec4c53ed272fe01e15914a9396e52
Sinan Ganiz <sinan.ganiz@gmail.com>
2025-05-09 14:21:10 +0300

    Initial project setup with core dependencies (Dapper, SQLite, FluentValidation, AutoMapper, Swagger)

---
+++ LibraryApi/Program.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
